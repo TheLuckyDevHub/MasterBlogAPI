94a61343c4951d6b9d01f6e52062289bcae45bbe
Mister Lucky <gluecksApplyJobs@gmail.com>
2025-11-25 14:38:02 +0100

    Added code for swagger API documentation and create masterblog.json

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -10,11 +10,27 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import logging
 import os
+from flask_swagger_ui import get_swaggerui_blueprint
 
 __IS_DEBUG = True
 
+
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
+
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 # Configure logging
 if __IS_DEBUG:
